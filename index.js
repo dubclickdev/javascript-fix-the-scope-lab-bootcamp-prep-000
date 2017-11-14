a7b2dd4beb185d482d6494c0370b340276a156bd
dubclickdev <battim_3c@yahoo.com>
2017-11-14 18:07:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function yourAnimal() {
   return animal
 }
 
-const two = 2
 function add2(n) {
 
   // Feel free to move things around!
+  const two = 2
   return n + two
 }
 
@@ -22,7 +22,7 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
-return theFunk {
+return theFunk() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
